c83ced3ced6d33ffbb61b96cfa48acf5eb6f7fba
Jean-Claude <jeanyao@ymail.com>
2023-01-28 12:44:42 +0300

    DOCTRINE RELATIONSHIPS - Cascade=remove orphanRemoval=true

--- features/migrations/Version20230128093914.php
+++ features/migrations/Version20230128093914.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230128091115 extends AbstractMigration
+final class Version20230128093914 extends AbstractMigration
 {
     public function getDescription(): string
     {
