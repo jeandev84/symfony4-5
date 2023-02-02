819ef2f1d4c578ac57082187145b21aefa0ac9d2
Jean-Claude <jeanyao@ymail.com>
2023-02-02 06:15:01 +0300

    Functional tests - send form

--- features/tests/DefaultControllerTest.php
+++ features/tests/DefaultControllerTest.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
@@ -9,14 +8,27 @@ class DefaultControllerTest extends WebTestCase
 {
     public function testSomething(): void
     {
+        $this->sendFormTests();
+    }
+
+
+    public function pageContainElement()
+    {
         $client = static::createClient();
         $crawler = $client->request('GET', '/app');
 
-        /*
         // dd($crawler->filter('h1')->text());
         # $this->assertResponseIsSuccessful();
         $this->assertSame(200, $client->getResponse()->getStatusCode());
         $this->assertSelectorTextContains('h1', $crawler->filter('h1')->text());
+
+    }
+
+    public function commonAssertionsTests()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/app');
+
 
         # Common assertions
         $this->assertGreaterThan(0, $crawler->filter('html:contains("Hello World")')->count()); // no passed
@@ -30,13 +42,13 @@ class DefaultControllerTest extends WebTestCase
             'Content-Type',
             'application/json'
         ),
-          'the "Content-Type" header is "application/json"' // optional message shown on failure
+            'the "Content-Type" header is "application/json"' // optional message shown on failure
         );
 
 
         $this->assertContains('foo', [$client->getResponse()->getContent()]);
 
-        $this->assertRegExp('/foo(bar)?/', $client->getResponse()->getContent()); assert Regular expression
+        $this->assertRegExp('/foo(bar)?/', $client->getResponse()->getContent()); // assert Regular expression
 
         $this->assertTrue($client->getResponse()->isSuccessful(), 'response status is 2xx');
         $this->assertTrue($client->getResponse()->isNotFound());
@@ -52,7 +64,13 @@ class DefaultControllerTest extends WebTestCase
 
         # Client is redirected him self
         $this->assertTrue($client->getResponse()->isRedirect());
-        */
+
+    }
+
+    public function clickToLinkTests()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/app');
 
 
         # Functional tests - click links
@@ -66,4 +84,23 @@ class DefaultControllerTest extends WebTestCase
 
         $this->assertContains('Remember me', [$client->getResponse()->getContent()]);
     }
+
+    public function sendFormTests()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/awesome-form');
+
+        $form = $crawler->selectButton('Sign in')->form();
+        $form['email'] = "user@demo.com";
+        $form['password'] = "user123";
+
+        $crawler = $client->submit($form);
+
+        // If I have successfully to login in application I'll be redirected to the home route
+        $crawler = $client->followRedirect();
+
+        // Verify if after login link we have logout link
+        $this->assertEquals(1, $crawler->filter('a:contains("logout")')->count());
+    }
+
 }
