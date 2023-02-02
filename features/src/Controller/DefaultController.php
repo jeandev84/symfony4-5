b4397458c81b95ddb060077f284243341e09b629
Jean-Claude <jeanyao@ymail.com>
2023-02-02 09:10:30 +0300

    Translation - Controller

--- features/src/Controller/DefaultController.php
+++ features/src/Controller/DefaultController.php
@@ -4,8 +4,13 @@ namespace App\Controller;
 use App\Repository\VideoRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+
 
 class DefaultController extends AbstractController
 {
@@ -13,9 +18,14 @@ class DefaultController extends AbstractController
 
     protected $entityManager;
 
-    public function __construct(EntityManagerInterface  $entityManager)
+    protected $eventDispatcher;
+
+    public function __construct(
+        EntityManagerInterface  $entityManager,
+        EventDispatcherInterface $eventDispatcher)
     {
           $this->entityManager = $entityManager;
+          $this->eventDispatcher = $eventDispatcher;
     }
 
 
@@ -23,8 +33,15 @@ class DefaultController extends AbstractController
     /**
      * @Route("/app", name="app")
     */
-    public function index(): Response
+    public function index(Request $request, TranslatorInterface $translator): Response
     {
+
+        $translated = $translator->trans('form.login.username');
+
+        dump($translated);
+        dump($request->getLocale());
+
+
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
